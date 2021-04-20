8e9e9503923e24e4e93057a3740959e970a12642
Shanmuganathanb <38834254+Shanmuganathanb@users.noreply.github.com>
2021-04-20 23:15:22 +0530

    Added Functional Store , index and about pages.

---
+++ index.js
@@ -0,0 +1,45 @@
+const cart_items = document.querySelector('#cart .cart-items');
+document.addEventListener('click',(e)=>{
+    
+    if (e.target.className=='shop-item-button'){
+        const id = e.target.parentNode.parentNode.id
+        const name = document.querySelector(`#${id} h3`).innerText;
+        const img_src = document.querySelector(`#${id} img`).src;
+        const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
+        let total_cart_price = document.querySelector('#total-value').innerText;
+        if (document.querySelector(`#in-cart-${id}`)){
+            alert('This item is already added to the cart');
+            return 
+        }
+        const cart_item = document.createElement('div');
+        cart_item.classList.add('cart-row');
+        cart_item.setAttribute('id',`in-cart-${id}`);
+        total_cart_price = parseFloat(total_cart_price) + parseFloat(price)
+        total_cart_price = total_cart_price.toFixed(2)
+        document.querySelector('#total-value').innerText = `${total_cart_price}`;
+        cart_item.innerHTML = `
+        <span class='cart-item cart-column'>
+        <img class='cart-img' src="${img_src}" alt="">
+            <span>${name}</span>
+    </span>
+    <span class='cart-price cart-column'>${price}</span>
+    <span class='cart-quantity cart-column'>
+        <input type="text" value="1">
+        <button>REMOVE</button>
+    </span>`
+        cart_items.appendChild(cart_item)
+    }
+
+    if (e.target.className=='purchase-btn'){
+        alert('Thanks for the purchase')
+        cart_items.innerHTML = ""
+        document.querySelector('#total-value').innerText = `0`;
+    }
+
+    if (e.target.innerText=='REMOVE'){
+        let total_cart_price = document.querySelector('#total-value').innerText;
+        total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2) ;
+        document.querySelector('#total-value').innerText = `${total_cart_price.toFixed(2)}`
+        e.target.parentNode.parentNode.remove()
+    }
+})
