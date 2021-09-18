b4befc5af8a9253b2f6cc926b852c66ad2d1435d
yash prasad <pyash@athenahealth.com>
2021-09-18 09:17:54 +0530

    Add and Get products Integrated

--- index.js
+++ index.js
@@ -68,3 +68,25 @@ parentContainer.addEventListener('click',(e)=>{
     }
 })
 
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get('http://localhost:3000/products').then((data)=> {
+        console.log(data);
+        if(data.request.status === 200) {
+            const products = data.data.products;
+            const parentSection = document.getElementById('Products');
+            products.forEach(product => {
+                const productHtml = `
+                <div>
+                    <h1>${product.title}</h1>
+                    <img src=${product.imageUrl}></img>
+                    <button> Add To Cart </button>
+
+                </div>`
+                parentSection.innerHTML =  parentSection.innerHTML +  productHtml;
+
+            })
+
+        }
+    })
+})
+
